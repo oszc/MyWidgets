a48bf756ecf4b8fc26227f600321ab46b855d733
JustinZhang@MacBook <oszcoszc@gmail.com>
2016-03-14 17:31:20 +0800

    kkjlsdfjlksadjflsdjflsaj

--- app/src/main/java/com/huuhoo/mywidgets/app/MainActivity.java
+++ app/src/main/java/com/huuhoo/mywidgets/app/MainActivity.java
@@ -1,18 +1,46 @@
 package com.huuhoo.mywidgets.app;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Point;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
 import com.huuhoo.mywidgets.app.utils.ImageUtil;
+import com.huuhoo.mywidgets.app.utils.Screen;
 
 public class MainActivity extends Activity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+    int res = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Point p = ImageUtil.calculateImageSize(new Point(100,100),new Point(200,100));
-        System.out.println("width:"+p.x + " height:"+p.y);
+      //  Point p = ImageUtil.calculateImageSize(new Point(100,100),new Point(200,100));
+      //  System.out.println("width:"+p.x + " height:"+p.y);
+        BitmapFactory.Options options = new BitmapFactory.Options();
+
+        options.inJustDecodeBounds = true;
+        res = R.drawable.hello;
+
+        BitmapFactory.decodeResource(getResources(), res,options);
+
+        Point imageSize = new Point(options.outWidth,options.outHeight);
+
+
+        Point targetSize= ImageUtil.calculateImageSize(new Point(Screen.getWidth(this), Screen.getHeight(this)), imageSize);
+
+        int sampleSize = ImageUtil.calculateSampleSize(this,res,targetSize);
+
+        options.inJustDecodeBounds = false;
+        options.inSampleSize = sampleSize;
+        Log.e(TAG,"sample size --> "+ sampleSize);
+
+        ImageView iv = (ImageView) findViewById(R.id.iv);
+        iv.setImageBitmap(BitmapFactory.decodeResource(getResources(),res,options));
+
+
     }
 }
--- app/src/main/java/com/huuhoo/mywidgets/app/utils/ImageUtil.java
+++ app/src/main/java/com/huuhoo/mywidgets/app/utils/ImageUtil.java
@@ -1,5 +1,8 @@
 package com.huuhoo.mywidgets.app.utils;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Point;
 
 /**
@@ -13,6 +16,11 @@ public class ImageUtil {
         if(canvas == null || image == null){
             return null;
         }
+
+        if(image.x < canvas.x && image.y < canvas.y){
+            return image;
+        }
+
         Point imageSize = new Point();
         if(image.y/(float)canvas.y >= image.x/(float)canvas.x){
             imageSize.y = canvas.y;
@@ -24,4 +32,47 @@ public class ImageUtil {
 
         return imageSize;
     }
+
+    public static int calculateSampleSize(String bmpPath , Point target){
+
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(bmpPath, options);
+        int height = options.outHeight;
+        int width = options.outWidth;
+
+        int looptime = 1;
+        while (height > target.y || width > target.x){
+
+            options.inSampleSize = (int)Math.pow(2,looptime++);
+            BitmapFactory.decodeFile(bmpPath,options);
+            height = options.outHeight;
+            width = options.outWidth;
+
+        }
+        return options.inSampleSize;
+
+
+    }
+
+    public static int calculateSampleSize(Context context ,int bmpPath , Point target){
+
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+         BitmapFactory.decodeResource(context.getResources(),bmpPath, options);
+        int height =options.outHeight;
+        int width = options.outWidth;
+        int looptime = 1;
+        while (height > target.y || width > target.x){
+            options.inSampleSize = (int)Math.pow(2,looptime++);
+            BitmapFactory.decodeResource(context.getResources(),bmpPath,options);
+            height = options.outHeight;
+            width = options.outWidth;
+        }
+        if(looptime > 2){
+            return (int)Math.pow(2,looptime-2);
+        }
+        return 1;
+    }
+
 }
